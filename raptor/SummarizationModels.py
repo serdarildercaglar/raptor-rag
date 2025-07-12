57fc7a852b6184a566d8bd0e5633fb2ef452e002
serdar <serdarildercaglar@gmail.com>
2025-07-12 17:03:32 +0300

    summarization upate

--- raptor/SummarizationModels.py
+++ raptor/SummarizationModels.py
@@ -72,3 +72,33 @@ class GPT3SummarizationModel(BaseSummarizationModel):
         except Exception as e:
             print(e)
             return e
+
+
+class GPT4OMiniSummarizationModel(BaseSummarizationModel):
+    def __init__(self, model="gpt-4o-mini"):
+
+        self.model = model
+
+    @retry(wait=wait_random_exponential(min=1, max=20), stop=stop_after_attempt(6))
+    def summarize(self, context, max_tokens=500, stop_sequence=None):
+
+        try:
+            client = OpenAI()
+
+            response = client.chat.completions.create(
+                model=self.model,
+                messages=[
+                    {"role": "system", "content": "You are a helpful assistant."},
+                    {
+                        "role": "user",
+                        "content": f"Write a summary of the following in Turkish, including as many key details as possible: {context}:",
+                    },
+                ],
+                max_tokens=max_tokens,
+            )
+
+            return response.choices[0].message.content
+
+        except Exception as e:
+            print(e)
+            return e
